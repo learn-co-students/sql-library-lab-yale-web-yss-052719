df01cd99ad79f0f146f775fae330bcfeee87115c
Peter <cirediatpl@gmail.com>
2019-06-04 17:41:10 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,50 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, year
+  FROM books
+  INNER JOIN series
+  ON books.series_id = series.id
+  WHERE series.id =(SELECT MIN(id) FROM series)
+  ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto
+  FROM characters
+  ORDER by motto ASC LIMIT 1"
+  ## why does string length ASC put them long to short
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY species
+  ORDER by COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  INNER JOIN series
+  ON authors.id = series.author_id
+  JOIN subgenres ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM characters
+  INNER JOIN series
+  ON characters.series_id = series.id
+  WHERE species =(SELECT MAX(species) FROM characters)
+  ORDER by series.title ASC LIMIT 1"
 end
+## probably need to fix above ORDER BY
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(character_books.id)
+  FROM characters
+  INNER JOIN character_books
+  ON characters.id = character_books.character_id
+  GROUP BY characters.name
+  ORDER BY COUNT(character_books.id) DESC"
 end
